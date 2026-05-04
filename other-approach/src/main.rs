07cdf264d48f7da2919e8b173fa7298ec261948d
Anders Rundgren <anders.rundgren.net@gmail.com>
2026-05-04 17:39:38 +0200

    cleaned-up Rc verified

--- other-approach/src/main.rs
+++ other-approach/src/main.rs
@@ -6,14 +6,12 @@ trait CoreTrait {
     fn to_string(&self) -> String;
 }
 
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct ArrayContent {
     vector: Rc<RefCell<Vec<CBOR>>>
 }
 
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct IntContent {
     value: i64
 }
@@ -28,7 +26,7 @@ impl CoreTrait for ArrayContent {
     fn to_string(&self) -> String {
         let mut string = String::new();
         string.push('[');
-        let array = (*self.vector.borrow()).clone();
+        let array = self.vector.borrow();
         let mut n = 0;
         while n < array.len() {
             if n > 0 {
@@ -42,10 +40,7 @@ impl CoreTrait for ArrayContent {
     }
 }
 
-impl ArrayContent {}
-
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum CBOR {
     Array(ArrayContent),
     Int(IntContent)
@@ -63,36 +58,43 @@ impl CBOR {
     pub fn get_i64(&self) -> i64 {
         match self {
             CBOR::Int(int_content) => int_content.value,
-            _ => panic!("Not an integer: CBOR::{:?}", self)
+            _ => panic!("Not an integer: CBOR::{}", self.type_name())
+        }
+    }
+
+    fn type_name(&self) -> &str {
+        match self {
+            CBOR::Array(_) => "Array",
+            CBOR::Int(_) => "Int"
         }
     }
 
     pub fn get(&self, index: usize) -> CBOR {
         match self {
-            CBOR::Array(map_content) => {
-                (*map_content.vector.borrow())[index].clone()
+            CBOR::Array(array_content) => {
+                (*array_content.vector.borrow())[index].clone()
             },
-            _ => panic!("Not an array : CBOR::{:?}", self)
+            _ => panic!("Not an array : CBOR::{}", self.type_name())
         }
     }
 
     pub fn add(&self, cbor_object: CBOR) -> CBOR {
         match self {
-            CBOR::Array(map_content) => {
-                map_content.vector.borrow_mut().push(cbor_object);
+            CBOR::Array(array_content) => {
+                array_content.vector.borrow_mut().push(cbor_object);
                 self.clone()
             },
-            _ => panic!("Not an array : CBOR::{:?}", self)
+            _ => panic!("Not an array : CBOR::{}", self.type_name())
         }
     }
 
     pub fn add_ref(&self, cbor_object: &CBOR) -> CBOR {
         match self {
-            CBOR::Array(map_content) => {
-                map_content.vector.borrow_mut().push(cbor_object.clone());
+            CBOR::Array(array_content) => {
+                array_content.vector.borrow_mut().push(cbor_object.clone());
                 self.clone()
             },
-            _ => panic!("Not an array : CBOR::{:?}", self)
+            _ => panic!("Not an array : CBOR::{}", self.type_name())
         }
     }
 
@@ -106,6 +108,15 @@ impl CBOR {
             CBOR::Int(core_trait) => core_trait
         }
     }
+
+    fn rc_count(&self) -> usize {
+        match self {
+            CBOR::Array(array_content) => {
+                Rc::strong_count(&array_content.vector)
+            },
+            _ => panic!("Not an array : CBOR::{:?}", self)
+        }
+    }
 }
 
 fn update_array(array: &CBOR) {
@@ -117,15 +128,23 @@ fn main() {
     let root_array = CBOR::new_array();
     update_array(&root_array);
     let an_integer = CBOR::new_i64(6);
-    let another_array: CBOR = CBOR::new_array();
-    another_array.add(CBOR::new_i64(567));
-    root_array.add_ref(&another_array);
-    another_array.add(CBOR::new_i64(888));
+    let sub_array: CBOR = CBOR::new_array();
+    sub_array.add(CBOR::new_i64(567));
+    root_array.add_ref(&sub_array);
+    sub_array.add(CBOR::new_i64(888));
     println!("integer = {}", an_integer.get_i64());
     root_array.add(an_integer).add(CBOR::new_i64(7));
     root_array.get(2).add(CBOR::new_i64(44));
     println!("root array: {}", root_array.to_string());
-    println!("another array: {}", another_array.to_string());
+    println!("another array: {}", sub_array.to_string());
     println!("integer = {}", root_array.get(2).get(1).get_i64());
+    assert_eq!(root_array.rc_count(), 1);
+    assert_eq!(sub_array.rc_count(), 2);
+    {
+        let y = sub_array.add(CBOR::new_i64(3));
+        assert_eq!(y.rc_count(), 3);
+        assert_eq!(sub_array.rc_count(), 3);
+    }
+    assert_eq!(sub_array.rc_count(), 2);
     root_array.get_i64();  // Panic!
 }
